4cfa558ad4bc4267dc7df592b7a333e4e9407e6e
novaeeken <novaeeken@Novas-MacBook-Pro.local>
2021-03-29 19:13:31 +0200

    Create solutions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NameItem from './components/NameItem';
 import './App.css';
 
 function App() {
+  const [activeName, setActiveName] = useState('');
+
+  useEffect(() => {
+    console.log("We gaan beginnen!");
+  }, [])
+
   return (
     <>
-      <h1>De actieve naam is: ...</h1>
+      <h1>De actieve naam is: {activeName}</h1>
       <ul>
-        <NameItem name="Henk" />
-        <NameItem name="Piet" />
-        <NameItem name="Jan" />
+        <NameItem name="Henk" activeName={activeName} setActiveName={setActiveName} />
+        <NameItem name="Piet" activeName={activeName} setActiveName={setActiveName} />
+        <NameItem name="Jan" activeName={activeName} setActiveName={setActiveName} />
       </ul>
     </>
   );
--- src/components/NameItem.js
+++ src/components/NameItem.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function NameItem({ name }) {
+function NameItem({ name, activeName, setActiveName }) {
+
+  useEffect(() => {
+    console.log(`${name} is in de DOM gezet!`);
+  }, [])
+
+  useEffect(() => {
+    if (activeName === name) {
+      console.log(`${name} wil graag een biertje bestellen.`);
+    }
+  }, [activeName])
+
   return (
     <li>
       <h3>{name}</h3>
-      <button type="button">
+      <button
+        type="button"
+        onClick={() => setActiveName(name)}
+      >
         Geef een rondje!
       </button>
     </li>
